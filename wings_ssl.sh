b0270204daa271c47f46184b7919d0a3e35be893
GenesisMiuss <miusssss@qq.com>
2024-09-11 16:46:37 +0800

    Create wings_ssl.sh

---
+++ wings_ssl.sh
@@ -0,0 +1,22 @@
+echo "正在为节点域名: $0 申请证书";
+echo "Ali_Key: $1";
+echo "Ali_Secret: $2";
+echo "Email: $3";
+
+# 安装 acme.sh
+git clone https://github.com/acmesh-official/acme.sh.git
+cd acme.sh
+./acme.sh --install -m $3
+
+# 添加阿里云accesskey
+export Ali_Key="$1"
+export Ali_Secret="$2"
+
+source ~/.bashrc
+
+mkdir -p /etc/letsencrypt/live/$0
+
+~/.acme.sh/acme.sh --set-default-ca --server zerossl
+
+# 申请证书
+~/.acme.sh/acme.sh --issue --dns dns_ali -d $0 --server zerossl --key-file /etc/letsencrypt/live/$0/privkey.pem --fullchain-file /etc/letsencrypt/live/$0/fullchain.pem --dnssleep 1000
